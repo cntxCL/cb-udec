ab3d80b0ada065337a27d963e8ca4a17921c21d5
Ricardo Cisterna <r.cisternasantos@gmail.com>
2016-12-09 00:32:37 -0300

    Proyecto model & migration

---
+++ database/migrations/2016_12_09_032914_create_proyectos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProyectosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proyectos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('nombre', 200);
+            $table->string('descripcion', 2000);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proyectos');
+    }
+}
