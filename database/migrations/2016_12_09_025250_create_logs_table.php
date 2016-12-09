be66bd3ff122dae77f3cceb6f7052f63f9dfc823
Ricardo Cisterna <r.cisternasantos@gmail.com>
2016-12-09 00:26:39 -0300

    Logs model & migration

---
+++ database/migrations/2016_12_09_025250_create_logs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('descripcion', 500);
+            $table->unsignedInteger('usuario_id');
+            $table->foreign('usuario_id')->references('id')->on('usuarios')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
