4706d02e7a4562d9bbd9d3dc010a0497dbc1dbf4
Gonzalo Candia <Gonzalo Candia>
2016-12-09 00:41:08 -0300

    admin-lte integrado

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('content')
+
+<div class="box box-primary">
+	<div class="box-header">
+		<h1 class="box-title">Bienvenido a Centro de Biotecnología UdeC</h1>
+	</div>
+	<div class="box-body">
+
+	</div>
+</div>
+
+@endsection
