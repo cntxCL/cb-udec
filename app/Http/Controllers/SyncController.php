e5673dd33ac1e00e747bb1f9ba87ca558074d0f6
Gonzalo Vergara A <go.vergara.a@gmail.com>
2016-12-10 12:17:35 -0300

    api beto mega alpha

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -3,13 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Personal;
 
 class SyncController extends Controller
 {
     //
 
-    public function syncPersonal()
+    public function addPersonal(Request $request)
     {
+    	$data = json_decode($request->input("data"));
+    	foreach ($data as $key => $value) {
+    		$personal = Personal::create($value);
+    		$personal->save();
+    	}
+    	return response()->json(["status" : "OK"])
+    }
 
+    public function deletePersonal(Request $request)
+    {
+    	$data = json_decode($request->input("data"));
+    	foreach ($data as $key => $value) {
+    		$personal = Personal::find($value);
+    		$personal->delete();
+    	}
+    	return response()->json(["status" : "OK"])
     }
 }
