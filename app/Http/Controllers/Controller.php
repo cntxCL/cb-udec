9f46d81b3243a9faec89976ea73a392554eb0a03
Ricardo Cisterna <r.cisternasantos@gmail.com>
2016-12-10 02:04:52 -0300

    lógica Logs implementada (en teoria, hay que probar)

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,14 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Instantiate a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('log')->only(['store', 'update', 'destroy']);
+    }
 }
---
+++ app/Http/Middleware/LogsRecorder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class LogsRecorder
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+
+    public function terminate($request, $response)
+    {
+        if($reponse->status() != 200) return;
+
+        $user = Auth::check() ? Auth::user()->nombre : "Usuario desconocido";
+        $controller = $request->route()->getController();
+        $action = $request->route()->getAction();
+
+        Log::create([
+            'descripcion' => $user . ' ' . $controller . '.' . $action,
+            'usuario_id' => Auth::check() ? Auth::id() : 1
+        ]);
+    }
+}
