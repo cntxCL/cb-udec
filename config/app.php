194a7a2572e2df5d551c35f518112cd5800f0a1c
Ricardo Cisterna <r.cisternasantos@gmail.com>
2017-04-21 23:40:15 -0300

    app configuration updated

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Laravel',
+    'name' => 'Centro de Biotecnología - UdeC',
 
     /*
     |--------------------------------------------------------------------------
@@ -64,7 +64,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'America/Santiago',
 
     /*
     |--------------------------------------------------------------------------
