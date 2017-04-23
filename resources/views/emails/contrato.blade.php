e6da476bec4ab12cde2e4073e131b19990ff179d
Gonzalo Vergara A <go.vergara.a@gmail.com>
2017-04-22 21:53:45 -0300

    configuración email y plantilla para correo próximo a vencer contrato

---
+++ resources/views/emails/contrato.blade.php
@@ -0,0 +1,6 @@
+<div>
+	<p>
+		El contrato de <span style="font-weight: bold">{{$item->personal->nombre}} {{$item->personal->apellido}}</span>
+		asociado al proyecto {{$item->proyecto->nombre}} vencerá en {{$item->fin->format("d/m/Y")}}
+	</p>
+</div>
