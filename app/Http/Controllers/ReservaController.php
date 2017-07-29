4be5e73f745fba9843ca3213e07efa4e996bb920
Ricardo Cisterna <r.cisternasantos@gmail.com>
2017-07-29 17:19:05 -0400

    bugfix envio correo

--- app/Http/Controllers/ReservaController.php
+++ app/Http/Controllers/ReservaController.php
@@ -89,7 +89,7 @@ class ReservaController extends Controller
 				$reserva->save();
 				$users = \App\User::where('tipo', 1)->where('activo', true)->get();
 	            foreach ($users as $user)
-	                Mail::to($reserva->responsable)->send(new ActivarReservaMail($reserva));
+	                Mail::to($user->email)->send(new ActivarReservaMail($reserva));
 	            Mail::to($reserva->responsable)->send(new ActivarReservaMail($reserva));
 			}
 			return view("reservas.edit", ['item' => $reserva]);
